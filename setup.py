e2e8b1467e04edade657cd8b69efaa932f25ffdc
d0ughnutz <126280474+d0ughnutz@users.noreply.github.com>
2023-04-27 16:08:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='api2sqldb',
     version='0.1.0',
-    # install_requires=['Your-Library'],
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     url='https://github.com/d0ughnutz/api2sqlbd.py',
     license='MIT',
     author='d0ughnutz',
-    author_email='dough.go00@email.com',
-    description='api2sqldb'
+    author_email='126280474+d0ughnutz@users.noreply.github.com',
+    description='Get crypto API data and store in a PosgreSQL DB'
 )
